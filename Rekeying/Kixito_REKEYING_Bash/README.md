5b108535b8789732056338ecf2ab6b5c82ad26f7
Kixitoo <97637534+Kixitoo@users.noreply.github.com>
2022-02-05 15:02:29 -0800

    Add files via upload

---
+++ Rekeying/Kixito_REKEYING_Bash/README.md
@@ -0,0 +1,28 @@
+# REKEYING BASH SUBMISSION BY KIXITO
+
+
+A Multi-Signature wallet enables transfer and management of funds involving multiple parties. For example, the implementation of a vault type lock with a 2 of 2 wallets requires 2 separate private keys to move funds or more complex approval mechanisms such as 3 signatures of 5 signers requiring any 3 keys out of 5.
+
+
+## RUN STEPS
+
+Create a test account for running the code.
+
+`git clone` this repository
+
+- `npm install ` to install algosdk 
+
+- cd into multisig.js and input your mnemonic, algod address and algod token respectively
+
+Initialize an algod client, set network params for transaction
+
+You'd need to visit https://bank.testnet.algorand.network/ to fund your multisig account
+
+Transaction ID - UJJNPG7IQYRIRRFF2ZZR7STDO6PYPMHKGBE2AEYX7XBYN5R44NNA
+
+
+GO TO   https://testnet.algoexplorer.io/tx/UJJNPG7IQYRIRRFF2ZZR7STDO6PYPMHKGBE2AEYX7XBYN5R44NNA to view TXN
+
+### YouTube Link
+
+[click here to watch](https://youtu.be/IJntaInN3H8)
---
+++ Rekeying/Kixito_REKEYING_Bash/multisig.js
@@ -0,0 +1,116 @@
+const algosdk = require('algosdk');
+
+const waitForConfirmation = async function (algodclient, txId, timeout) {
+    
+    if (algodclient == null || txId == null || timeout < 0) {
+        throw "Bad arguments.";
+    }
+    let status = (await algodclient.status().do());
+    if (status == undefined) throw new Error("Unable to get node status");
+    let startround = status["last-round"] + 1;
+    let currentround = startround;
+
+    while (currentround < (startround + timeout)) {
+        let pendingInfo = await algodclient.pendingTransactionInformation(txId).do();
+        if (pendingInfo != undefined) {
+            if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+                //Got the completed Transaction
+                return pendingInfo;
+            }
+            else {
+                if (pendingInfo["pool-error"] != null && pendingInfo["pool-error"].length > 0) {
+                    // If there was a pool error, then the transaction has been rejected!
+                    throw new Error("Transaction Rejected" + " pool error" + pendingInfo["pool-error"]);
+                }
+            }
+        }
+        await algodclient.statusAfterBlock(currentround).do();
+        currentround++;
+    }
+    throw new Error("Transaction not confirmed after " + timeout + " rounds!");
+};
+
+const token = {
+    "X-API-Key": "nq1M8RDrIK6CqmXvgdctW1mvuhvgI3nLO2bAk8Aa"
+}
+const server = "https://testnet-algorand.api.purestake.io/ps2";
+const port = "";
+
+const keypress = async () => {
+    process.stdin.setRawMode(true)
+    return new Promise(resolve => process.stdin.once('data', () => {
+        process.stdin.setRawMode(false)
+        resolve()
+    }))
+}
+
+(async () => {
+  
+    let account1_mnemonic = "minute hawk cruise mother hub labor health have random grit run since ostrich armed space double piano shove spawn lens slim urge traffic above crater";
+    let account2_mnemonic = "name spin gasp cheese entry rotate matter way mosquito heavy tumble piece bike miss mercy any level level urban coyote mom stumble addict abstract fitness";
+    let account3_mnemonic = "lunch slush exclude adjust burden win ramp retire ladder ladder distance delay keen midnight please term predict where hawk stick obey cycle cradle absorb biology"
+
+    let account1 = algosdk.mnemonicToSecretKey(account1_mnemonic);
+    let account2 = algosdk.mnemonicToSecretKey(account2_mnemonic);
+    let account3 = algosdk.mnemonicToSecretKey(account3_mnemonic);
+    console.log(account1.addr);
+    console.log(account2.addr);
+    console.log(account3.addr);
+
+    // Setup the parameters for the multisig account
+    const mparams = {
+        version: 1,
+        threshold: 2,
+        addrs: [
+            account1.addr,
+            account2.addr,
+            account3.addr,
+        ],
+    };
+
+    let multsigaddr = algosdk.multisigAddress(mparams);
+    console.log("Multisig Address: " + multsigaddr);
+    //Pause execution to allow using the dispenser on testnet to put tokens in account
+    console.log('Dispense funds to this account on TestNet https://bank.testnet.algorand.network/');
+    // await keypress();
+    try {
+        let algodclient = new algosdk.Algodv2(token, server, port);
+
+        // Get the relevant params from the algod
+        let params = await algodclient.getTransactionParams().do();
+        // comment out the next two lines to use suggested fee
+        //params.fee = 1000;
+        //params.flatFee = true;
+
+        const receiver = account3.addr;
+        let names = '{"firstName":"Kixito", "lastName":"Bash"}';
+        const enc = new TextEncoder();
+        const note = enc.encode(names);
+
+
+        let txn = algosdk.makePaymentTxnWithSuggestedParams(multsigaddr, receiver, 1000000, undefined, note, params);
+        let txId = txn.txID().toString();
+        // Sign with first signature
+
+        let rawSignedTxn = algosdk.signMultisigTransaction(txn, mparams, account1.sk).blob;
+        //sign with second account
+        let twosigs = algosdk.appendSignMultisigTransaction(rawSignedTxn, mparams, account2.sk).blob;
+        //submit the transaction
+        await algodclient.sendRawTransaction(twosigs).do();
+
+        // Wait for confirmation
+        let confirmedTxn = await waitForConfirmation(algodclient, txId, 4);
+        //Get the completed Transaction
+        console.log("Transaction " + txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+        let mytxinfo = JSON.stringify(confirmedTxn.txn.txn, undefined, 2);
+        console.log("Transaction information: %o", mytxinfo);
+        let string = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+        console.log("Note field: ", string);
+        const obj = JSON.parse(string);
+        console.log("Note first name: %s", obj.firstName);
+
+
+    } catch (err) {
+        console.log(err.message);
+    }
+})().then(process.exit)
