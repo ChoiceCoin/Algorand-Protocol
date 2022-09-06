24e0a6b237472817a0f691c8a799a1eca5ec5786
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2022-09-06 14:06:45 -0700

    Update walletconnect.js

--- React-Wallet-Connect/walletconnect.js
+++ React-Wallet-Connect/walletconnect.js
@@ -1,2 +1,33 @@
 // Easy and simplified JS software for connecting Pera Wallet.
 
+// Imports
+import './App.css';
+
+// Wallet Connect
+function WalletConnect() {
+  const wallet = () => {
+    console.log('Connect')
+  }
+  return(
+    <button onClick={wallet}>Connect Wallet</button>
+  )
+};
+
+// React functions must return a React component
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Choice Coin Connect
+        </h1>
+        <div>
+          <WalletConnect />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
