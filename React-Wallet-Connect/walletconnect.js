466ca3bf1214a5b875c36058e0cbc731727e5132
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2022-09-07 20:39:07 -0700

    Update walletconnect.js

--- React-Wallet-Connect/walletconnect.js
+++ React-Wallet-Connect/walletconnect.js
@@ -1,28 +1,77 @@
 // Imports
 import './App.css';
+import algosdk from "algosdk";
+import { PeraWalletConnect } from "@perawallet/connect";
+import { useEffect } from 'react';
+
+// perawallet instantiating
+const perawallet = new PeraWalletConnect()
+
+// algoClient
+const algod_token = {
+  "X-API-Key": "" 
+}
+const algod_address = "";
+const headers = "";
+const algodClient = new algosdk.Algodv2(algod_token, algod_address, headers);
+
+//get address
+const address = localStorage.getItem('address');
+
+// converter address
+const converterAddress = ''
+
+//asset id
+const ASSET_ID = '';
+
+/// transaction code
+const  transaction = async () => {
+  const suggestedParams = await algodClient.getTransactionParams().do();
+  const ethAddress = document.getElementById('reciever').value
+  const enc = new TextEncoder();
+  const note = enc.encode('Transaction with perawallet:'+ ethAddress);
+  const txn = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+    from: address,
+    to: converterAddress,
+    amount: 1,
+    assetIndex: ASSET_ID,
+    note : note,
+    suggestedParams,
+  });
+  const optInTxn = [{txn : txn, signers: [address]}]
+  const signedTxn = await perawallet.signTransaction([optInTxn])
+    await algodClient.sendRawTransaction(signedTxn).do();
+  console.log('error')
+}
 
 // Wallet Connect
-function WalletConnect() {
-  const wallet = () => {
-    console.log('Connect')
+async function walletConnect() {
+  const newAccounts= await perawallet.connect()
+  localStorage.setItem("address", newAccounts[0]);
+  window.location.reload()
+  console.log('Connect')
   }
-  return(
-    <button onClick={wallet}>Connect Address</button>
-  )
-};
 
-// Wallet Disconnect
-function WalletDisconnect() {
-  const wallet = () => {
-    console.log('Disconnect')
-  }
-  return(
-    <button onClick={wallet}>Disconnect Address</button>
-  )
-};
+// wallet disconnect
+const disconnect = () => {
+  perawallet.disconnect()
+  localStorage.removeItem("address");
+  window.location.reload()
+}
 
 // React functions must return a React component
 function App() {
+  useEffect(() => {
+    perawallet.reconnectSession().then((accounts) => {
+      if (accounts.length) {
+        localStorage.setItem("address", accounts[0]);
+      }
+      perawallet.connector?.on("disconnect", () => {
+        localStorage.removeItem("address");
+      });
+    })
+    .catch((e) => console.log(e));
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -30,10 +79,15 @@ function App() {
           Choice Coin Connect
         </h1>
         <div>
-          <WalletConnect />
+          <button id='button1' onClick={walletConnect}> Connect</button>
         </div>
+        <p></p>
         <div>
-          <WalletDisconnect />
+          <button id='button1' onClick={transaction}> Send</button>
+        </div>
+        <p></p>
+        <div>
+          <button id='button1' onClick={disconnect}> Disconnect</button>
         </div>
       </header>
     </div>
